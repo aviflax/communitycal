739b72500a4232c37a97188a05e593dd3d618101
Avi Flax <avi@aviflax.com>
2025-09-02 23:03:10 -0400

    ical: start on generating iCalendar documents

---
+++ system/src/communitycal/ical.clj
@@ -0,0 +1,39 @@
+(ns communitycal.ical
+  (:require
+   [communitycal.temporals :refer [date->zdt]])
+  (:import
+   (net.fortuna.ical4j.model Calendar)
+   (net.fortuna.ical4j.model.component VEvent)
+   (net.fortuna.ical4j.model.property Description XProperty)))
+
+(def company-name "Calendrical")
+(def product-name "CommunityCal")
+(def language "EN")
+(def product-id (format "-//%s//%s//%s" company-name product-name language))
+
+(defn make-calendar
+  [nom]
+  (-> (Calendar.)
+      (.withProdId product-id)
+      (.withDefaults)
+      (.withProperty (XProperty. "X-WR-CALNAME" nom))
+      (.getFluentTarget)))
+
+(defn make-event
+  [{:event/keys [name start end timezone-id notes]}]
+  (-> (VEvent.
+        (date->zdt start timezone-id)
+        (date->zdt end timezone-id)
+        name)
+      (.withProperty (Description. notes))
+      (.getFluentTarget)))
+
+(comment
+  (make-calendar "Foo Bar")
+
+  (make-event #:event{:name "Foo"
+                      :notes "Bar"
+                      :start (java.util.Date.)
+                      :end (java.util.Date.)
+                      :timezone-id "America/New_York"})
+  ,)
